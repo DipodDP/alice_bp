f784a7cdb016583b7bcc65bbf015295ebc9d7429
DipodDP <dipoddp@gmail.com>
2025-10-29 17:26:03 +0700

    feat: Add User and AccountLinkToken models to admin
    This commit registers the `User` and `AccountLinkToken` models with the Django admin site, making them manageable through the admin interface.

--- alice_skill/admin.py
+++ alice_skill/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BloodPressureMeasurement
+from .models import BloodPressureMeasurement, User, AccountLinkToken
 
 
 @admin.register(BloodPressureMeasurement)
@@ -7,3 +7,17 @@ class BloodPressureMeasurementAdmin(admin.ModelAdmin):
     list_display = ("user_id", "systolic", "diastolic", "measured_at")
     list_filter = ("user_id", "measured_at")
     search_fields = ("user_id", "systolic", "diastolic")
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("alice_user_id", "telegram_user_id", "timezone", "created_at")
+    list_filter = ("timezone",)
+    search_fields = ("alice_user_id", "telegram_user_id")
+
+
+@admin.register(AccountLinkToken)
+class AccountLinkTokenAdmin(admin.ModelAdmin):
+    list_display = ("telegram_user_id", "used", "created_at", "expires_at")
+    list_filter = ("used",)
+    search_fields = ("telegram_user_id",)
